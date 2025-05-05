c0e2c8c87939c5209e9c32e3149852ac5997d030
Fayaz409 <ftunio06@gmail.com>
2025-05-05 22:37:30 +0500

    Add Custom Cookies

--- cookiesdemo/cookiesapp/urls.py
+++ cookiesdemo/cookiesapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns =[
     path('all-cookies/',views.cookies_demo,name='all-cookies'),
+    path('create-cookie/',views.add_cookie,name='add-cookie'),
 ]
--- cookiesdemo/cookiesapp/views.py
+++ cookiesdemo/cookiesapp/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 
@@ -6,3 +8,12 @@ def cookies_demo(request):
     cookies = request.COOKIES
 
     return render(request,'cookiesapp/cookies.html',{'cookies':cookies})
+
+def add_cookie(request):
+    if request.method=='POST':
+        cookie_name= request.POST.get('cookie_name')
+        cookie_value= request.POST.get('cookie_value')
+        response  = HttpResponseRedirect(reverse('all-cookies')) 
+        response.set_cookie(cookie_name,cookie_value,120)
+        return response
+    return JsonResponse({'message':'Invalid request Method'})
