8399ad840ecdf55af9973b1dbc1161dcc8913455
Fayaz409 <ftunio06@gmail.com>
2025-05-05 23:22:51 +0500

    read and delete cookies

--- cookiesdemo/cookiesapp/urls.py
+++ cookiesdemo/cookiesapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns =[
     path('all-cookies/',views.cookies_demo,name='all-cookies'),
     path('create-cookie/',views.add_cookie,name='add-cookie'),
-    path('delete-all-cookies/',views.clear_cookies,name='delete-cookies')
+    path('delete-all-cookies/',views.clear_cookies,name='delete-cookies'),
+    path('read-cookie/<str:cookie_name>',views.read_cookies,name='read-cookie'),
+    path('delete-cookie/<str:cookie_name>',views.delete_cookie,name='delete-cookie')
 ]
--- cookiesdemo/cookiesapp/views.py
+++ cookiesdemo/cookiesapp/views.py
@@ -24,3 +24,12 @@ def clear_cookies(request):
     for key in request.COOKIES:
         response.delete_cookie(key)
     return response
+
+def read_cookies(request,cookie_name):
+    cookie_value  = request.COOKIES.get(cookie_name)
+    return render(request,'cookiesapp/read_cookie.html',{'cookie_name':cookie_name,'cookie_value':cookie_value})
+
+def delete_cookie(request,cookie_name):
+    response = HttpResponseRedirect(reverse('all-cookies'))
+    response.delete_cookie(cookie_name)
+    return response
